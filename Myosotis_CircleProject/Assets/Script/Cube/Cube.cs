930f1318689979eea510e380d79ed760054a12ab
Vijeinath Tissaveerasingham <vijeinath.tissaveerasingham@students.fhnw.ch>
2016-11-18 16:31:28 +0100

    First Verion
    Verschieben des Projektes

---
+++ Myosotis_CircleProject/Assets/Script/Cube/Cube.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+using TouchScript.Behaviors;
+using System;
+
+public class Cube : MonoBehaviour {
+	private TransformGesture gesture;
+	private bool touchIsHappening = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		print ("ScreenPosition x: "+ gesture.ScreenPosition.x);
+		if (touchIsHappening) {
+			print ("Cube touching");
+			Vector2 targetPosition = Camera.main.ScreenToWorldPoint (new Vector2 (gesture.ScreenPosition.x, gesture.ScreenPosition.y));
+			transform.position = targetPosition;
+		}
+	}
+
+	void Update(){
+		//print ("ScreenPosition x: "+ gesture.ScreenPosition.x);
+	}
+
+	private void OnEnable()
+	{
+		gesture = GetComponent<TransformGesture> ();
+		gesture.TransformStarted += HandleTransformStarted;
+		gesture.TransformCompleted += HandleTransformCompleted;
+	}
+
+	private void OnDisable()
+	{
+		gesture = GetComponent<TransformGesture> ();
+		gesture.TransformStarted -= HandleTransformStarted;
+		gesture.TransformCompleted -= HandleTransformCompleted;
+	}
+
+	void HandleTransformStarted (object sender, EventArgs e)
+	{
+		touchIsHappening = true;		
+	}
+
+	void HandleTransformCompleted (object sender, EventArgs e)
+	{
+		touchIsHappening = false;	
+	}
+}
---
+++ Myosotis_CircleProject/Assets/Script/StarBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+using TouchScript.Behaviors;
+using System;
+
+public class StarBehaviour : MonoBehaviour {
+
+	private TransformGesture gesture;
+	private Transform rb2d;
+	private bool isActive = false;
+	public float attractionStrength = 10;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Transform> ();
+	}
+
+
+	void Update () {
+		if (isActive && !float.IsNaN(gesture.ScreenPosition.x)) {
+			// get the positions of this object and the target
+			Vector2 targetPosition = Camera.main.ScreenToWorldPoint (new Vector2 (gesture.ScreenPosition.x, gesture.ScreenPosition.y));
+			Vector2 myPosition = transform.position;
+
+			// work out direction and distance
+			Vector2 direction = (targetPosition - myPosition).normalized;
+			float distance = Vector3.Magnitude (targetPosition - myPosition);
+
+			// apply square root to distance if specified to do so in the inspector
+			//if (useSqrtOfDistance) distance = Mathf.Sqrt(distance);
+
+			Vector3 resultingForceAmount = attractionStrength * direction * distance;
+
+			// then finally add the force to the rigidbody
+			rb2d.position =  targetPosition;
+		}
+	}
+
+	private void OnEnable()
+	{
+		gesture = GetComponent<TransformGesture> ();
+		gesture.TransformStarted += HandleTransformStarted;
+		gesture.TransformCompleted += HandleTransformCompleted;
+	}
+
+	private void OnDisable()
+	{
+		gesture.TransformStarted -= HandleTransformStarted;
+		gesture.TransformCompleted -= HandleTransformCompleted;
+	}
+
+	void HandleTransformStarted (object sender, EventArgs e)
+	{
+		isActive = true;		
+	}
+
+	void HandleTransformCompleted (object sender, EventArgs e)
+	{
+		isActive = false;	
+	}
+}
