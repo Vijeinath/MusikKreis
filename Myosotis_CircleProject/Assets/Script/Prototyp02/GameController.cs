d73f4caa0bc07f8411dc9e24a328cd8aaecac195
Vijeinath Tissaveerasingham <vijeinath.tissaveerasingham@students.fhnw.ch>
2016-11-29 23:36:58 +0100

    Prototyp 3

---
+++ Myosotis_CircleProject/Assets/Script/Prototyp02/GameController.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+	public ParticleSystem circlePs;
+
+	private List<AudioClip>[] audioSource;
+	private Star_P02 star1;
+	private Star_P02 star2;
+	private Star_P02 star3;
+	private Star_P02 star4;
+	private CircleSmall circle;
+	private GameObject circleBig;
+	private AudioSource source;
+	private List<AudioClip> listOfSounds;
+	private int randomInt; 
+
+	private int degree = 0;
+	private int degreeDelta = 14;
+	private float radius = 1.3f;
+
+	// Use this for initialization
+	void Start () {
+		//Stars referenzieren
+		star1 = GameObject.Find ("Star 01").GetComponent<Star_P02>();
+		star2 = GameObject.Find ("Star 02").GetComponent<Star_P02>();
+		star3 = GameObject.Find ("Star 03").GetComponent<Star_P02>();
+		star4 = GameObject.Find ("Star 04").GetComponent<Star_P02>();
+		circle = GameObject.Find ("Circle Small").GetComponent<CircleSmall>();
+		circleBig = GameObject.Find ("Circle Big");
+
+		Color temp = circleBig.GetComponent<SpriteRenderer> ().color;
+		temp.a = 0f;
+		circleBig.GetComponent<SpriteRenderer> ().color = temp;
+
+		//Sounds hinzufügen
+		audioSource = new List<AudioClip>[7];
+		for (int i = 0; i<audioSource.Length; i++){
+			audioSource[i] = new List<AudioClip> ();
+		}
+			
+		audioSource[0].Add (Resources.Load("Flute/flute01") as AudioClip);
+		audioSource[0].Add (Resources.Load("Flute/flute02") as AudioClip);
+		audioSource[0].Add (Resources.Load("Flute/flute03") as AudioClip);
+		audioSource[1].Add (Resources.Load("Guitar/guitar01") as AudioClip);
+		audioSource[1].Add (Resources.Load("Guitar/guitar02") as AudioClip);
+		audioSource[1].Add (Resources.Load("Guitar/guitar03") as AudioClip);
+		audioSource[2].Add (Resources.Load("Harpe/harpe01") as AudioClip);
+		audioSource[2].Add (Resources.Load("Harpe/harpe02") as AudioClip);
+		audioSource[2].Add (Resources.Load("Harpe/harpe03") as AudioClip);
+		audioSource[3].Add (Resources.Load("Piano/piano01") as AudioClip);
+		audioSource[3].Add (Resources.Load("Piano/piano02") as AudioClip);
+		audioSource[3].Add (Resources.Load("Piano/piano03") as AudioClip);
+		audioSource[4].Add (Resources.Load("Sax/sax01") as AudioClip);
+		audioSource[4].Add (Resources.Load("Sax/sax02") as AudioClip);
+		audioSource[4].Add (Resources.Load("Sax/sax03") as AudioClip);
+		audioSource[5].Add (Resources.Load("Trumpet/trumpet01") as AudioClip);
+		audioSource[5].Add (Resources.Load("Trumpet/trumpet02") as AudioClip);
+		audioSource[5].Add (Resources.Load("Trumpet/trumpet03") as AudioClip);
+		audioSource[6].Add (Resources.Load("Xylophone/xylophone01") as AudioClip);
+		audioSource[6].Add (Resources.Load("Xylophone/xylophone01") as AudioClip);
+		audioSource[6].Add (Resources.Load("Xylophone/xylophone01") as AudioClip);
+
+
+		star1.clip = audioSource [0] [1];
+		star2.clip = audioSource [0] [0];
+		star3.clip = audioSource [0] [2];
+		star4.clip = audioSource [4] [2];
+		star4.isWrongStar = true;
+
+		//Zum Testen
+		//source = GetComponent<AudioSource> ();
+		//source.PlayOneShot(audioSource[6][1], 1F);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		DrawCircle ();
+		if (circle.isfound){
+			NewAudios ();
+			circle.isfound = false;
+		}
+
+
+	}
+
+	void DrawCircle(){
+		circlePs.transform.position = new Vector2 (Mathf.Cos (degree * Mathf.PI / 180)*radius,Mathf.Sin (degree * Mathf.PI / 180)*radius);
+		circlePs.Play ();
+		degree = (degree + degreeDelta) %360;
+	}
+
+	public void NewAudios (){
+		var tempRandom = UnityEngine.Random.Range(0,7);
+		while (tempRandom == randomInt) {
+			tempRandom = UnityEngine.Random.Range(0,7);
+		}
+		randomInt = tempRandom;
+
+		switch (randomInt) {
+		case 0:
+			star1.clip = audioSource [3] [2];
+			star2.clip = audioSource [0] [2];
+			star3.clip = audioSource [3] [0];
+			star4.clip = audioSource [3] [1];
+			star4.isWrongStar = false;
+			star1.isWrongStar = false;
+			star2.isWrongStar = true;
+			star3.isWrongStar = false;
+			break;
+		case 1:
+			star1.clip = audioSource [6] [1];
+			star2.clip = audioSource [1] [2];
+			star3.clip = audioSource [1] [0];
+			star4.clip = audioSource [1] [1];
+			star4.isWrongStar = false;
+			star1.isWrongStar = true;
+			star2.isWrongStar = false;
+			star3.isWrongStar = false;
+			break;
+		case 2:
+			star1.clip = audioSource [6] [1];
+			star2.clip = audioSource [6] [2];
+			star3.clip = audioSource [2] [0];
+			star4.clip = audioSource [6] [0];
+			star4.isWrongStar = false;
+			star1.isWrongStar = false;
+			star2.isWrongStar = false;
+			star3.isWrongStar = true;
+			break;
+		case 3:
+			star1.clip = audioSource [0] [1];
+			star2.clip = audioSource [0] [0];
+			star3.clip = audioSource [0] [2];
+			star4.clip = audioSource [4] [2];
+			star4.isWrongStar = true;
+			star1.isWrongStar = false;
+			star2.isWrongStar = false;
+			star3.isWrongStar = false;
+			break;
+		case 4:
+			star1.clip = audioSource [3] [1];
+			star2.clip = audioSource [2] [2];
+			star3.clip = audioSource [2] [0];
+			star4.clip = audioSource [2] [1];
+			star4.isWrongStar = false;
+			star1.isWrongStar = true;
+			star2.isWrongStar = false;
+			star3.isWrongStar = false;
+			break;
+		case 5:
+			star1.clip = audioSource [5] [1];
+			star2.clip = audioSource [5] [2];
+			star3.clip = audioSource [5] [0];
+			star4.clip = audioSource [1] [1];
+			star4.isWrongStar = true;
+			star1.isWrongStar = false;
+			star2.isWrongStar = false;
+			star3.isWrongStar = false;
+			break;
+		case 6:
+			star1.clip = audioSource [4] [1];
+			star2.clip = audioSource [4] [2];
+			star3.clip = audioSource [0] [0];
+			star4.clip = audioSource [4] [0];
+			star4.isWrongStar = false;
+			star1.isWrongStar = false;
+			star2.isWrongStar = false;
+			star3.isWrongStar = true;
+			break;
+		default:
+			break;		
+		}
+	}
+
+
+}
---
+++ Myosotis_CircleProject/Assets/Script/Prototyp02/Star_P02.cs
@@ -0,0 +1,185 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+using TouchScript.Behaviors;
+using System;
+using System.Collections.Generic;
+
+public class Star_P02 : MonoBehaviour {
+
+	//Public Attributes
+	public ParticleSystem collisionParticle;
+	public ParticleSystem trailParticle;
+	public AudioClip sound;
+	public bool isWrongStar = false;
+
+	//Constants
+	private Vector3 startPoint = new Vector3 (0.3f, 0.3f);
+	private Vector2 strength = new Vector2 (7.0f, 7.0f);
+	private float circleRadius = 1.2f;
+
+	private TransformGesture gesture;
+	private Rigidbody2D rb2d;
+	private AudioSource source;
+	public AudioClip clip;
+	private List<AudioClip> listOfSounds;
+	private bool touchIsHappening = false;
+	private bool isInSmallCircle = false;
+
+	private int degree;
+	private float scalePixel = 0.0007f;
+	private int deltaDegree = 2;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		source = GetComponent<AudioSource> ();
+		listOfSounds = new List<AudioClip> ();
+		LoadAudioClips (listOfSounds);
+		degree = UnityEngine.Random.Range(0,360);
+	}
+
+	public virtual void LoadAudioClips(List<AudioClip> listOfSounds){
+		listOfSounds.Add (Resources.Load("Sound/luteJazz") as AudioClip);
+		listOfSounds.Add (Resources.Load("Sound/classicfunk01") as AudioClip);
+		listOfSounds.Add (Resources.Load("Sound/classicfunk02") as AudioClip);
+		listOfSounds.Add (Resources.Load("Sound/2cuteBells") as AudioClip);
+		listOfSounds.Add (Resources.Load("Sound/oldStringRise") as AudioClip);
+		listOfSounds.Add (Resources.Load("Sound/discoGuitar") as AudioClip);
+		listOfSounds.Add (Resources.Load("Sound/harpe01") as AudioClip);
+		listOfSounds.Add (Resources.Load("Sound/harpe02") as AudioClip);
+		listOfSounds.Add (Resources.Load("Sound/cavaleria") as AudioClip);
+	}
+
+	private void OnEnable()
+	{
+		gesture = GetComponent<TransformGesture> ();
+		gesture.TransformStarted += HandleTransformStarted;
+		gesture.TransformCompleted += HandleTransformCompleted;
+	}
+
+	private void OnDisable()
+	{
+		gesture.TransformStarted -= HandleTransformStarted;
+		gesture.TransformCompleted -= HandleTransformCompleted;
+	}
+
+	void HandleTransformStarted(object sender, EventArgs e)
+	{
+		touchIsHappening = true;
+	}
+
+	void HandleTransformCompleted (object sender, EventArgs e)
+	{	
+		touchIsHappening = false;
+	}
+
+
+	// All the physic stuffs
+	void FixedUpdate () 
+	{	
+		if (touchIsHappening) {
+			Vector2 targetPosition = Camera.main.ScreenToWorldPoint (new Vector2 (gesture.ScreenPosition.x, gesture.ScreenPosition.y));
+			transform.position = targetPosition;
+
+			trailParticle.transform.position = targetPosition;
+			trailParticle.Play ();
+
+		} else {
+			rb2d.drag = 2.2f;
+			float distanceLength = this.transform.position.magnitude;
+			Vector2 objectDistance = this.transform.position - startPoint;
+
+			if ((distanceLength > circleRadius) && !isInSmallCircle)
+			{
+				rb2d.AddForce (-7 * objectDistance);
+			
+				trailParticle.transform.position = transform.position;
+				trailParticle.Play ();
+			} /*else if ((distanceLength < circleRadius) && (isMoving)){
+				print ("Rotate");
+				transform.rotation = Quaternion.AngleAxis(bla, new Vector3(0,10,0));
+				bla = bla + 15.0f;
+			}
+			*/
+		}
+
+	}
+
+	public void AForce(Vector2 v2){
+		rb2d.AddForce (-7 * v2);
+	}
+
+	//Partikel- und Soundstart nur nach Kreiseintritt
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.gameObject.tag == "Circle_Small") {
+			isInSmallCircle = true;
+		}
+
+		if ((touchIsHappening) && !isCollisionOutside()) {
+			
+		}
+
+		if (!touchIsHappening && isCollisionOutside()){
+			collisionParticle.transform.position = this.transform.position;
+			collisionParticle.Play ();	
+
+			var randomInt = UnityEngine.Random.Range(0,listOfSounds.Count);
+			//source.PlayOneShot(listOfSounds[randomInt], 1F);
+			source.PlayOneShot(clip, 1F);
+		}
+	}
+
+	//Rotationsträgheit der Sterne werden erhöht nach Kollision 
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Star") {
+			coll.gameObject.GetComponent<Rigidbody2D> ().angularDrag = 1.5f;
+			rb2d.angularDrag = 1.5f;
+
+			//Funtkioniert noch nicht
+			/*
+			ContactPoint2D bla = new ContactPoint2D();
+			print ("X:"+bla.point.x + "? Y: " + bla.point.y);
+			*/
+
+			//Funktioniert, aber sieht blöd aus
+			/*
+			Vector3 distance = coll.rigidbody.transform.position - this.transform.position;
+			distance =  Vector3.Scale(new Vector2(0.5f,0.5f), distance);
+			Vector3 collisionPoint = this.transform.position+distance;
+			collisionParticle.transform.position = collisionPoint;
+			collisionParticle.Play ();
+			*/
+		}
+	}
+
+	void Update(){
+		//Moves Star up and down if it is still
+		float length = rb2d.velocity.magnitude;
+		if ((length <0.1f) &&(length>-0.1f)) {
+			float bla = Mathf.Sin (degree * Mathf.PI / 180) * scalePixel;
+			float delta = transform.position.y + bla;
+			transform.position = new Vector3(transform.position.x,delta);
+			degree = (degree + deltaDegree) % 360;
+		}
+	}
+
+	bool isCollisionOutside(){
+		return this.transform.position.magnitude > circleRadius;
+	}
+
+	//
+	void OnCollisionExit2D(Collision2D coll) {
+		
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Circle_Small") {
+			isInSmallCircle = false;
+		}
+	}
+
+}
